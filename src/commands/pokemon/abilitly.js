6ebbb41d111626c611be0ee77f350f63b7ceab4f
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-08-21 17:27:20 -0500

    comments, yo

--- src/commands/pokemon/abilitly.js
+++ src/commands/pokemon/abilitly.js
@@ -28,6 +28,6 @@ module.exports = class SayCommand extends Command {
 		    • Name: ${abilities.BattleAbilities[text].name}
             • Description: ${abilities.BattleAbilities[text].desc}
             • Rating: ${abilities.BattleAbilities[text].rating}
-		`);
+		`); // the rating part might break things, I haven't tested it yet
     }
 };
