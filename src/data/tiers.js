7393acc7886650f2ff28017556353b34cc3da728
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-09-02 16:02:07 -0500

    start of code for tierlists

---
+++ src/data/tiers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let BattleTiers = {
+	"AG": {
+		fullName: "Anything Goes",
+		desc: "No Pokemon are banned",
+		restrictions: ["Sleep Clause", "idk"]
+	},
+	"Uber": {
+		fullName: "Ubers",
+		desc: "All Pokemon except for Mega Rayquaza are allowed",
+		restrictions: ["Sleep Clause", "idk"]
+	},
+	"OU": {
+		fullName: "Overused",
+		desc: "not the top dudes.",
+		restrictions: ["Sleep Clause", "idk"]
+	},
+}
